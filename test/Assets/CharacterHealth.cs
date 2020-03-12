4b1cc3e8188c0c2bb71e9c576365ff2012007780
Vijay GameArt <vj.rajan@yoobeecolleges.com>
2020-03-13 12:37:28 +1300

    Github101
    Uploading test unity project with updated gitignore

---
+++ test/Assets/CharacterHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour
+{
+    public int PlayerHealth = 3;
+    int damage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(PlayerHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision Enemycollision)
+    {
+        if (Enemycollision.gameObject.tag == "Enemy")
+        {
+            PlayerHealth -= damage;
+            print("Collided with enemy" + PlayerHealth);
+        }
+    }
+}
